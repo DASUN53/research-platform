ce09679c57413cbb7a66ef0400e4d022ea57d8cf
Luthira Peiris <22cse0369@ms.sab.ac.lk>
2026-06-28 20:07:39 +0530

    updated the file

--- backend/routes/reputationRoutes.js
+++ backend/routes/reputationRoutes.js
@@ -6,3 +6,12 @@ import {
   recalculateUserBadges,
   recalculateAllBadges,
 } from "../controllers/reputationController.js";
+
+const router = express.Router();
+
+router.get("/leaderboard", getLeaderboard);
+router.get("/user/:userId", getUserReputation);
+router.post("/user/:userId/recalculate-badges", recalculateUserBadges);
+router.post("/recalculate-badges", recalculateAllBadges);
+
+export default router;
