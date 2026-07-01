69fe381f7d7e8aa8a4610cce12c4d75b054ce539
Luthira Peiris <22cse0369@ms.sab.ac.lk>
2026-07-01 17:22:10 +0530

    updated the App.jsx file

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,10 +1,30 @@
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
+import { applyTheme, getSavedTheme } from "./utils/theme";
 
-export default function App() {
-  return (
-    <div className="size-full">
-      <RouterProvider router={router} />
-    </div>
-  );
+function App() {
+  useEffect(() => {
+    applyTheme(getSavedTheme());
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const handleSystemThemeChange = () => {
+      const savedTheme = getSavedTheme();
+
+      if (savedTheme === "system") {
+        applyTheme("system");
+      }
+    };
+
+    mediaQuery.addEventListener("change", handleSystemThemeChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleSystemThemeChange);
+    };
+  }, []);
+
+  return <RouterProvider router={router} />;
 }
+
+export default App;
