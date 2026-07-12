ee3a9c478d9fbc73c29124e0402a7b976aca910a
Dasun53 <dasununimail@gmail.com>
2026-07-12 07:50:55 +0530

    admin-routes-1

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,3 +1,22 @@
 import express from "express";
 
 import { protect, adminOnly } from "../middleware/authMiddleware.js";
+
+import {
+  getAllUsers,
+  deleteUser,
+  getAllPosts,
+  deletePost,
+  archivePost,
+  getAllComments,
+  deleteComment,
+  getAllSolutions,
+  deleteSolution,
+  getAllFields,
+  createField,
+  updateField,
+  deleteField,
+  getArchive,
+  restoreArchivePost,
+} from "../controllers/adminController.js";
+
